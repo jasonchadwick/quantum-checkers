70a42670e83e4ca02ef82dca2551444c9371b9ef
Jason Chadwick <jasoncha@andrew.cmu.edu>
2021-03-05 12:32:07 -0500

    individual probability distributions

--- README.md
+++ README.md
@@ -3,3 +3,11 @@
 Checkers, but each "piece" is a probability distribution that spreads out over time.
 
 The winner is the first to drop their opponent's total probability score below 1 piece.
+
+### Todo
+
+- each piece is separate, instead of all your pieces being a single probability distribution
+- AI to play against
+- UI
+    - pie chart representation of each probability?
+    - option to look at the individual prob. distributions of each piece
--- quantum-checkers.py
+++ quantum-checkers.py
@@ -5,40 +5,64 @@ import random
 letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
 
 def init_board(size):
-    b2 = np.zeros((size, size))
-    for r in range(int(size/2) - 1):
-        r_even = r % 2 == 0
-        for c in range(size):
-            c_even = c % 2 == 0
-            if (r_even and c_even) or ((not r_even) and not c_even):
-                b2[r][c] = 1
-    b1 = np.zeros((size, size))
+    b0 = []
     for r in range(round(size/2) + 1, size):
         r_even = r % 2 == 0
         for c in range(size):
             c_even = c % 2 == 0
             if (r_even and c_even) or ((not r_even) and not c_even):
-                b1[r][c] = 1
-    return [b1, b2]
+                board = np.zeros((size, size))
+                board[r][c] = 1
+                b0.append(board)
+    b1 = []
+    for r in range(int(size/2) - 1):
+        r_even = r % 2 == 0
+        for c in range(size):
+            c_even = c % 2 == 0
+            if (r_even and c_even) or ((not r_even) and not c_even):
+                board = np.zeros((size, size))
+                board[r][c] = 1
+                b1.append(board)
+    return [np.array(b0), np.array(b1)]
+
+def expected(player_board, r, c):
+    prob_no_piece = 1
+    for piece in player_board:
+        prob_no_piece *= 1-piece[r][c]
+    return 1 - prob_no_piece
+
+def expected_vals(board):
+    size = len(board[0][0])
+    expected_array = [np.zeros((size, size)), np.zeros((size, size))]
+    for player,b in enumerate(board):    
+        for r in range(size):
+            for c in range(size):
+                prob_no_piece = 1
+                for piece in b:
+                    prob_no_piece *= 1-piece[r][c]
+                expected_array[player][r][c] = expected(b, r, c)
+    return expected_array
 
 def print_board(board):
-    size = len(board[0])
+    size = len(board[0][0])
+    expected = expected_vals(board)
     print('')
+    print('  ' + ''.join(['   ' + str(i) + '   ' for i in range(1,9)]))
     print('  ' + '-' * (7 * size))
     for r in range(size):
         for c in range(size):
             if c == 0:
                 print(letters[r] + ' | ', end='')
-            val = board[0][r][c]
+            val = expected[0][r][c]
             if val == 0:
                 print('     | ', end='')
             else:
                 print(f'{Fore.GREEN}%.2f{Style.RESET_ALL} | ' % val, end='')
-        print()
+        print(letters[r])
         for c in range(size):
             if c == 0:
                 print('  | ', end='')
-            val = board[1][r][c]
+            val = expected[1][r][c]
             if val == 0:
                 print('     | ', end='')
             else:
@@ -51,37 +75,60 @@ def print_board(board):
 def do_timestep(board, n=1, spreading=0.5):
     for step in range(n):
         init_bs = np.copy(board)
-        size = len(board[0])
-        for i,b in enumerate(board):
+        size = len(board[0][0])
+        for player,b in enumerate(board):
             for r in range(size):
                 for c in range(size):
-                    initial_val = init_bs[i][r][c]
-                    if initial_val != 0:
-                        possible_expansions = [(r-1,c-1), (r-1,c+1),
-                                               (r+1,c-1), (r+1,c+1)]
-                        neighbor_spread = initial_val * spreading / 4
-                        for e in possible_expansions:
-                            if e[0] >= 0 and e[0] < size and e[1] >= 0 and e[1] < size:
-                                b[r][c] -= neighbor_spread
-                                b[e[0]][e[1]] += neighbor_spread
+                    for piece_num,piece in enumerate(b):
+                        initial_val = init_bs[player][piece_num][r][c]
+                        if initial_val != 0:
+                            possible_expansions = [(r-1,c-1), (r-1,c+1),
+                                                   (r+1,c-1), (r+1,c+1)]
+                            neighbor_spread = initial_val * spreading / 4
+                            for e in possible_expansions:
+                                if e[0] >= 0 and e[0] < size and e[1] >= 0 and e[1] < size:
+                                    piece[r][c] -= neighbor_spread
+                                    piece[e[0]][e[1]] += neighbor_spread
 
-def measure(val0, val1):
-    rolls = [1, 1]
-    while(rolls[0] and rolls[1]):
-        rolls[0] = int(random.random() < val0)
-        rolls[1] = int(random.random() < val1)
-    return rolls
+def score_piece(p):
+    return min(1, sum(p.flatten()))
+
+def score_board(b):
+    s0 = round(sum(b[0].flatten()), 2)
+    s1 = round(sum(b[1].flatten()), 2)
+    print('_' * 60)
+    print('Score:')
+    print('  Green: ' + str(s0))
+    print('  Red:   ' + str(s1))
+    return [s0, s1]
 
 def measure_board(board):
-    # TODO: each piece has its own prob. distribution. This will ensure conservation
-    # of pieces if they have not been jumped. Will be a big rewrite process.
-    size = len(board[0])
-    for r in range(size):
-        for c in range(size):
-            rolls = measure(board[0][r][c], board[1][r][c])
-            board[0][r][c] = rolls[0]
-            board[1][r][c] = rolls[1]
+    # Right now, pieces can be measured on top of one another - should this be allowed?
+    size = len(board[0][0])
+    cart_prod = np.transpose([np.tile([0,1], len(board[0])), np.repeat(range(len(board[0])), len([0,1]))])
+    np.random.shuffle(cart_prod)
+    cart_prod = [tuple(x) for x in cart_prod]
+
+    for player,piece in cart_prod:
+        prob_no_piece = 1 - score_piece(board[player][piece])
+        if prob_no_piece < 0:
+            print('WTF')
+            print(prob_no_piece)
+        reshape = np.append(board[player][piece].reshape(size*size), prob_no_piece)
+        location = np.random.choice(range(len(reshape)), p=reshape)
+        location_filled = np.array([int(x == location) for x in range(size*size)])
+        board[player][piece] = location_filled.reshape(size,size)
+        #print(board[player][piece])
     return board
+
+def inspect_board(board, player, piece):
+    npieces = len(board[0])
+    size = len(board[0][0])
+    displayboard = [np.zeros((npieces, size, size)), np.zeros((npieces, size, size))]
+    displayboard[player][piece] = board[player][piece]
+    print_board(displayboard)
+    input('Press enter to go back to full board')
+    print_board(board)
 
 def player_move(board, player):
     valid_move = False
@@ -92,15 +139,19 @@ def player_move(board, player):
             print(f'{Fore.RED}Player ' + str(player) + f'\'s turn.{Style.RESET_ALL}')
 
         print('Move format ex: \"a1 b3\" jumps from A1 to B3')
-        move1 = input('Input move (p to pass, m to measure): ')
+        print('Type \"i 6\" to inspect your 6th piece')
+        move1 = input('Input move (p to pass, m to measure, i to inspect): ')
         
         if move1 == 'p':
             print('Player ' + str(player) + ' passed.')
             return
         elif move1 == 'm':
-            measure_board(board)
+            board = measure_board(board)
             print('Player ' + str(player) + ' has measured the board! ')
             return
+        elif move1[0] == 'i'[0]:
+            inspect_board(board, player, int(move1[2]))
+            continue
         r1 = ord(move1[0]) - 97
         c1 = int(move1[1]) - 1
 
@@ -119,35 +170,29 @@ def player_move(board, player):
         opponent = 1 - player
 
         b = board[player]
-        b1 = board[opponent]
+        b_x = board[opponent]
 
-        src = b[r1][c1]
-        src1 = b1[r1][c1]
-        dst = b[r2][c2]
-        dst1 = b1[r2][c2]
+        for p in b:
+            src = p[r1][c1]
+            dst = p[r2][c2]
+            dst_sum = expected(b, r2, c2)
+            dst_x = expected(b_x, r2, c2)
 
-        if attempted_move:
-            print('Move')
-            prob_no_move = dst + dst1
-            b[r2][c2] = (1 - prob_no_move) * src + dst
-            b[r1][c1] = prob_no_move * src
-        
-        elif attempted_jump:
-            print('Jump')
-            jmp = [int((r1 + r2) / 2), int((c1 + c2) / 2)]
-            prob_jump = b1[jmp[0]][jmp[1]] * (1 - (dst + dst1))
-            b[r2][c2] = prob_jump * src + dst
-            b[r1][c1] = (1-prob_jump) * src
-            b1[jmp[0]][jmp[1]] = (1-prob_jump) * b1[jmp[0]][jmp[1]]
+            if attempted_move:
+                prob_no_move = dst_sum + dst_x
+                p[r2][c2] = (1 - prob_no_move) * src + dst
+                p[r1][c1] = prob_no_move * src
+            
+            elif attempted_jump:
+                jmp = [int((r1 + r2) / 2), int((c1 + c2) / 2)]
+                prob_jump = expected(b_x,jmp[0],jmp[1]) * (1 - (dst_sum + dst_x))
+                print(prob_jump)
+                p[r2][c2] = prob_jump * src + dst
+                p[r1][c1] = (1-prob_jump) * src
+                print(p[r1][c1])
+                for p_x in b_x:
+                    p_x[jmp[0]][jmp[1]] = (1-prob_jump) * p_x[jmp[0]][jmp[1]]
 
-def score_board(b):
-    s0 = round(sum([sum(r) for r in b[0]]), 2)
-    s1 = round(sum([sum(r) for r in b[1]]), 2)
-    print('_' * 60)
-    print('Score:')
-    print('  Green: ' + str(s0))
-    print('  Red:   ' + str(s1))
-    return [s0, s1]
 
 def play(size=8, spreading=0.1):
     playing = True
@@ -173,7 +218,11 @@ def play(size=8, spreading=0.1):
             win(1)
         elif (score[1] < 1):
             win(0)
-    
     return
+
+#b=init_board(8)
+#score_board(b)
+#do_timestep(b, spreading=0.1)
+#print_board(b)
 
 play()
